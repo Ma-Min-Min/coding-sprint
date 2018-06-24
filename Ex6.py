be92fa9f28b37c7ceae147430b689593b8dd71e0
Ma-Min-Min <minmin12697@gmail.com>
2018-06-24 15:48:04 +0630

    Added Ex6.py

--- Ex6.py
+++ Ex6.py
@@ -14,6 +14,6 @@ joke_evaluation = "Isn't that joke so funny?! {}"
 
 print(joke_evaluation.format(hilarious))
 
-w= "This is the left side of..."
-e= "a string with a right side."
+w="This is the left side of..."
+e="a string with a right side."
 print(w+e)
